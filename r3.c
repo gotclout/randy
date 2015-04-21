9be78ef4bee922e7ef1cfafc0e2da314f3e58982
General <rlfosterjr@gmail.com>
2015-04-21 12:29:07 -0400

    Create r3.c
    This is the modified project c file

---
+++ r3.c
@@ -0,0 +1,18 @@
+#ifndef __r3__
+#define __r3__
+
+#include "sys.h"
+
+#define MAX_ARG 1024
+
+/**
+ * Contains data associated with a command to be executed
+ */
+typedef struct _CmdData
+{
+  char* args[MAX_LEN], *pth, *inf, *outf; //args, path to exec io redirect
+  int argc , plen; //num args
+  char* strcmd;
+} CmdData;
+
+#endif
