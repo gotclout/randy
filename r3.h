d40c4e5e4572ab773073e2567afdc3dc28b5ecad
General <rlfosterjr@gmail.com>
2015-04-29 17:04:47 -0400

    Update r3.h

--- r3.h
+++ r3.h
@@ -1,7 +1,8 @@
 #ifndef __r3__
 #define __r3__
 
-#include "sys.h"
+#include "sys.h"   //for using system implementation
+#include <ctype.h> //implicit declaration of isspace
 
 #define MAX_ARG 1024
 
@@ -11,7 +12,7 @@
 typedef struct _CmdData
 {
   char* args[MAX_LEN], *pth, *inf, *outf; //args, path to exec io redirect
-  int argc , plen; //num args
+  int argc , plen;                        //num args
   char* strcmd;
 } CmdData;
 
