dcf04fc1e1fea91e4b0a42b47c2dd7cc839fb33d
General <rlfosterjr@gmail.com>
2015-04-29 17:03:00 -0400

    Update sys.c

--- sys.c
+++ sys.c
@@ -1,12 +1,13 @@
 /**
- * SIDHU-HW2.c -- This program implements the system function int system(const char*comm)
+ * SIDHU-HW2.c -- This program implements the system function
+ * int system(const char*comm)
  */
 
-/*Name: Randeep Sidhu
-Date: 3/26/2015
-Class: ISA563
-
-*/
+/**
+ * Name: Randeep Sidhu
+ * Date: 3/26/2015
+ * Class: ISA563
+ */
 
 #include "sys.h"
 #include <unistd.h>
@@ -21,6 +22,7 @@ Class: ISA563
 #include <stdbool.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <ctype.h> //implicit declaration of isspace warning
 
 #define MAX_LEN 256 //defining the max length of the arguments
 
@@ -62,7 +64,7 @@ int tokenize(char **pcomm, char* args[], char** outfile, char** infile, int* fla
 
       if(strcmp(args[argc],">")==0)                 //if you encounter the outfile operator
       {
-        token=strtok(0," ");                        //set the token to null ended as we don't care what is after the output file operator
+        token=strtok(0," ");                        //set the token to null ended, don't care what is after the outfile operator
         *outfile=(char*)malloc(strlen(token));      //allocate memory for the token
         memset(*outfile, 0, strlen(token) + 1);     //set the memory for the token for the output file
         strcpy(*outfile, token);                    //copy output file into token
@@ -94,11 +96,11 @@ int tokenize(char **pcomm, char* args[], char** outfile, char** infile, int* fla
         }
       }
       else
-        argc++;                                    //if you don't encounter any operators you keep on incrementing argc
+        argc++;                                    //if encountered no operators keep on incrementing argc
     }
   }
 
-  return(argc);                                    //return the amount of arguments being passed
+  return argc;                                     //return the amount of arguments being passed
 }
 
 /**
@@ -106,43 +108,43 @@ int tokenize(char **pcomm, char* args[], char** outfile, char** infile, int* fla
  */
 int system(const char *comm)
 {
-  char* args[MAX_LEN];                       //Defining MAX # of arguments can be
-  memset(args, 0, sizeof(char*) * MAX_LEN);  //Setting the memory for these arguments
+  char* args[MAX_LEN];                      //Defining MAX # of arguments can be
+  memset(args, 0, sizeof(char*) * MAX_LEN); //Setting the memory for these arguments
 
-  int rval = EXIT_SUCCESS;                   //the return value of success if everything completes
+  int rval = EXIT_SUCCESS;                  //the return value of success if everything completes
 
-  const char* cmd = "sh";                    //first argument system() calls is sh
-  args[0]=(char*)malloc(strlen(cmd));        //allocating memory for args[0]
-  strcpy(args[0], cmd);                      //copying cmd into args[0]
+  const char* cmd = "sh";                   //first argument system() calls is sh
+  args[0]=(char*)malloc(strlen(cmd));       //allocating memory for args[0]
+  strcpy(args[0], cmd);                     //copying cmd into args[0]
 
-  const char* arg0="-c";                     //2nd argument system() calls is -c
+  const char* arg0="-c";                    //2nd argument system() calls is -c
 
-  args[1]=(char*)malloc(strlen(arg0));       //allocating memory for insertion into array args for -c
-  strcpy(args[1], arg0);                     //copying into args[1] the command -c
-  char* infile=0,*outfile=0;                 //infile and outfile being used for input and output files
+  args[1]=(char*)malloc(strlen(arg0));      //allocating memory for insertion into array args for -c
+  strcpy(args[1], arg0);                    //copying into args[1] the command -c
+  char* infile=0,*outfile=0;                //infile and outfile being used for input and output files
+  size_t len = strlen(comm);                //setting the length to the size of the command.
   int p = 0;
-  size_t len = strlen(comm);                 //setting the length to the size of the command.
-  for( p=0; p < len && comm[p] != ' '; p++)  //for loop to go through the command
+  for(  ; p < len && comm[p] != ' '; p++)   //for loop to go through the command
   {
     //no arguments needed inside loop
   }
 
   int flag = 0;
 
-  char* co = (char*) malloc(strlen(comm) + 1);         //since system accepts a constant need to copy comm to another
-                                                       // variable called co
+  char* co = (char*) malloc(strlen(comm) + 1);//since system accepts a constant need to copy comm to another
+                                              // variable called co
   memset(co, 0, strlen(comm)+1);
-  strncpy(co, comm, strlen(comm));                     //strncpy copies full string up to the length of the string
-  int argc=tokenize(&co,args,&outfile,&infile,&flag);  //calling the tokenize function you pass *co instead of comm.
-  bool waitfor=1;       //waiting for a child process or not
-  bool redirectin = 0;  //bool for whether you are redirecting input in
+  strncpy(co, comm, strlen(comm));            //strncpy copies full string up to the length of the string
+  tokenize(&co,args,&outfile,&infile,&flag);  //calling the tokenize function you pass *co instead of comm.
+
+  bool waitfor     = 1; //waiting for a child process or not
+  bool redirectin  = 0; //bool for whether you are redirecting input in
   bool redirectout = 0; //bool for whether you are redirecting output out
 
   pid_t child;
   int cstatus; //exit status of child
   pid_t c;     //pid of child to be returned by the wait
 
-  //int j = 0;
   if(flag > 0)
   {
     args[flag]=0;
@@ -150,15 +152,14 @@ int system(const char *comm)
   int ifid=0, ofid=0;
 
   const char* a = *(args+2); //setting char* a to args +2
-
-  if((child= fork())==0)     //if forked
+  if((child= fork())==0)     //if forked child process
   {
-    if(redirectout==1)       //if you are redirecting the command to an output file
+    if(redirectout==1)       //if redirecting the command to an output file
     {
       close(1);              //close the file to make sure it wasn't being used
-      int ofid = open(outfile, O_RDONLY|O_WRONLY|O_CREAT, 00666); //setting a variable to the open file command
-      if(ofid==-1)                                                //if the return value from from ofid is -1
-        fprintf(stderr, "Could not open %s\n",outfile);           //return an error that you couldn't open the file
+      int ofid = open(outfile, O_RDONLY|O_WRONLY|O_CREAT, 00666);//setting a variable to the open file command
+      if(ofid==-1)                                               //if the return value from from ofid is -1
+        fprintf(stderr, "Could not open %s\n",outfile);          //return an error that you couldn't open the file
     }
     if(redirectin==1)                                             //if you are redirecting output
     {
@@ -169,7 +170,7 @@ int system(const char *comm)
     }
 
     char* cc = *(args+3);  //setting char* c = *(args+3)
-    execlp(a,"",cc);       //calling the execlp command and passing the arguments to it.
+    execlp(a,"",cc, NULL);       //calling the execlp command and passing the arguments to it.
     if(redirectin)         //if output is being redirected in
     {
       close (0);           //close 0 is being used for a child
@@ -238,3 +239,4 @@ int system(const char *comm)
 
   return (0);
 }*/
+
